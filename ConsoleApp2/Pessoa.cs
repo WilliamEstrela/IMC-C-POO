3c5c8569480e5ea4390c68f5873ba6661bc2df05
William Estrela Louzeiro <wel.estrela@gmail.com>
2017-09-04 16:10:09 -0300

    primeiro commit

---
+++ ConsoleApp2/Pessoa.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Person
+    {
+        public int age;
+        private float height;//altura
+        private float weight;//peso
+        private float imc;
+        
+
+        public Person(float height, float weight, int age=0)
+        {
+            this.height = height;//altura
+            this.weight = weight;//peso
+            this.age = age;
+            this.calcImc();
+        }
+
+        public float getHeight()
+        {
+            return this.height;
+        }
+
+        public float getWeight()
+        {
+            return this.weight;
+        }
+
+        public float getImc()
+        {
+            return this.imc;
+        }
+
+        public void setHeight(float height)
+        {
+            this.height = height;
+            this.calcImc();
+        }
+
+        public void setWeight(float weight)
+        {
+            this.weight = weight;
+            this.calcImc();
+        }
+
+        private void calcImc()
+        {
+            this.imc = this.weight / (this.height * this.height);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Person Jhon = new Person(1.86f,105f);//altura e peso
+           
+      
+            Console.WriteLine("Idade de Jhon " + Jhon.age);
+            Jhon.age = 21;
+            Console.WriteLine("Nova idade de Jhon " + Jhon.age);
+
+            //p.imc = 21.45 
+
+            Console.WriteLine("Height " + Jhon.getHeight() + " and ewight " + Jhon.getWeight());
+
+
+            Person Peter = new Person(height: 1.86f, weight: 105f, age: 19);
+            Console.WriteLine("Idade de peter " + Peter.age);
+
+            Console.WriteLine("Jhon IMC is " + Jhon.getImc());
+
+            Jhon.setWeight(80);
+
+            Console.WriteLine("ImC de jhon eh: " +Jhon.getImc());
+
+        }
+    }
+}
